0f5a4b4045f9465dba32b1c4d065fbc3196684c6
lofayo <870876711@qq.com>
2017-09-07 21:54:37 +0800

    complished in dev

--- functions.js
+++ functions.js
@@ -1,3 +1,20 @@
 // 这是一个master分支，所谓的master分支，只能提交没有bug的代码
 // 从现在开始，由你和另一位小伙伴联合开发一些功能函数
 // coding .......
+
+// 在dev下开发，当函数能正常运行了，再和并到master分支上
+function youPlay() {
+    var winH = $(window).height();
+    $("video").each(function() {
+        var posTop = $(this).offset().top;
+        var scrollH = $(window).scrollTop(); //浏览器滚动高度
+        var targetH = $(this).height();
+        var needShow = (posTop - winH + winH / 2) <= scrollH && scrollH <= (posTop - winH + winH / 2 + targetH);
+        if (needShow) {
+            $('video').each(function() {
+                $(this)[0].pause();
+            })
+            $(this)[0].play();
+        }
+    })
+}
